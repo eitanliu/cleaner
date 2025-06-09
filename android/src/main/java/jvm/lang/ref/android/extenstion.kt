55f4cf8f068cce31e7073fa01b4328b1b3a09338
eitanliu <eitanliu@gmail.com>
2025-06-10 02:57:26 +0800

    extensions.kt

--- android/src/main/java/jvm/lang/ref/android/extenstion.kt
+++
@@ -1,7 +0,0 @@
-package jvm.lang.ref.android
-
-inline fun <T> Cleaner.registerObject(
-    resource: T, crossinline builder: (resource: T) -> Runnable
-) {
-    register(resource, builder(resource))
-}
